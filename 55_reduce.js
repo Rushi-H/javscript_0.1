aaef26cb21ec1ce03c96baa1b3aff3bf31a3bb45
Rushi-h <rushihonkande@gmail.com>
2026-03-06 17:12:26 +0530

    updated

--- 55_reduce.js
+++ 55_reduce.js
@@ -4,3 +4,11 @@ const output = arr.reduce((res,curr) =>{
     return res+curr;
 });
 console.log(output);
+
+
+// find largest number 
+
+const findLargerNumber = arr.reduce((prev,curr) => {
+    return prev > curr ? prev : curr;
+});
+console.log(findLargerNumber);
